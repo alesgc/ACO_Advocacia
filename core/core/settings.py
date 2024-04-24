c963725d152179dcad1f9818cef3963521889023
alesgc <88426812+alesgc@users.noreply.github.com>
2024-04-24 16:00:36 -0300

    Production v1.1.1

--- core/core/settings.py
+++ core/core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^u&ec$cvg6f=34*n-=#7jm@d^dx(0tq23fna4w=h7)brz9nx=*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
